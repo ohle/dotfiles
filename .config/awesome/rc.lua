de0a38882e16712c9ada9271cc63bd10d2a19351
Ohle Claussen <joclaussen@gmail.com>
2017-04-26 10:15:26 +0200

    remove unused code

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -360,9 +360,6 @@ awful.rules.rules = {
         { class = { "Mplayer", "gimp" },
           name = { "QEMU" } },
           properties = { floating = true } } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
 }
 -- }}}
 
